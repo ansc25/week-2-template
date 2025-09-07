6331ea0f8c89e27cb82bec6afd5a64561e11cd13
Anurag Subedi <ansc25@lehigh.edu>
2025-09-07 02:19:10 +0000

    testing part 1 graph

--- student_code.py
+++ student_code.py
@@ -2,10 +2,18 @@
 
 node_dict={'a':0,'b':1,'c':2,'d':3,'e':4}
 
-# test2
-
 def part_1_graph():
-    pass
+    """
+    Returns the part 1 graph represented as a list of sets.
+    """
+    graph = [
+        {1, 4},  # a -> b, e
+        {2},     # b -> c
+        {3, 4},  # c -> d, e
+        {1},     # d -> b
+        set()    # e -> (no outgoing edges)
+    ]
+    return graph
 
 def part_2_graph():
     pass
