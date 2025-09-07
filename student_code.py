f767cef1b35a07aafac5d1778c15aa2a44d4a6cb
Anurag Subedi <ansc25@lehigh.edu>
2025-09-07 02:58:16 +0000

    implemented part 1 to 5

--- student_code.py
+++ student_code.py
@@ -7,22 +7,63 @@ def part_1_graph():
     Returns the part 1 graph represented as a list of sets.
     """
     graph = [
-        {1, 4},  # a -> b, e
-        {2},     # b -> c
-        {3, 4},  # c -> d, e
-        {1},     # d -> b
-        set()    # e -> (no outgoing edges)
+        {'b', 'e'},      # a
+        {'c'},           # b
+        {'d', 'e'},      # c
+        {'b'},           # d
+        set()            # e
     ]
     return graph
 
 def part_2_graph():
-    pass
+    """
+    Returns the part 2 graph represented as a list of lists.
+    """
+    graph = [
+        ['a', 'b', 'e'],     # a
+        ['c'],               # b
+        ['a', 'd', 'e'],     # c
+        [],                  # d
+        ['d']                # e
+    ]
+    return graph
 
 def part_3_graph():
-    pass
+    """
+    Returns the part 3 graph (weighted) represented as a list of dicts with weights.
+    """
+    graph = [
+        {'a': 8, 'b': 1, 'e': 4},  # a
+        {'c': 3},                  # b
+        {'a': 2, 'e': 4},          # c
+        {},                        # d
+        {}                         # e
+    ]
+
+    return graph
 
 def part_4_graph():
-    pass
+    """
+    Returns the part 4 graph represented as a dict of sets.
+    """
+    graph = {
+        'a': {'a', 'b', 'e'},
+        'b': {'c'},
+        'c': {'a'},
+        'd': set(),
+        'e': set()
+    }
+    return graph
 
 def part_5_graph():
-    pass
+    """
+    Returns the weighted part 5 graph represented as a dict of dicts.
+    """
+    graph = {
+        'a': {'b': 5},
+        'b': {'e': 3},
+        'c': {},
+        'd': {},
+        'e': {'a': 6, 'b': 2}
+    }
+    return graph
